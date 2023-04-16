b1e9382c4004a477382dfbf8339f9b765d80d757
Izna <izna8402@gmail.com>
2023-04-16 12:12:25 +0530

    push

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const moment = require('moment');
 const simpleGit= require('simple-git');
 const FILE_PATH = './data.json';
 const makeCommit= (x,y) =>{
-    const DATE= moment().subtract(1,'y').add(1,'d').add(x,'w').add(y,'d').format();
+    const DATE= moment().subtract(2,'y').add(1,'d').add(x,'w').add(y,'d').format();
     const data= {
         date: DATE
     }
