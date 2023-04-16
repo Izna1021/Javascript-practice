8eb1509b1fc8bb1e5729d8768f75a909d9fee235
Izna <izna8402@gmail.com>
2023-04-16 11:26:55 +0530

    2 months

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const moment = require('moment');
 const simpleGit= require('simple-git');
 const FILE_PATH = './data.json';
 
-const DATE= moment().subtract(1,'m').add(1,'d').format();
+const DATE= moment().subtract(2,'m').add(1,'d').format();
     const data= {
         date: DATE
     }
