a2ed414fdb0ccefcae898495d0226622fdc1dc27
Izna <izna8402@gmail.com>
2023-04-16 10:53:51 +0530

    2 change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const simpleGit= require('simple-git');
 const FILE_PATH = './data.json';
 const makeCommit=(x,y) =>{
     const DATE= moment().subtract(1,'y').add(1,'d')
-                    add(x,'w').add(y,'d').format();
+                    .add(x,'w').add(y,'d').format();
     const data= {
         date: DATE
     }
